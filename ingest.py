5c23fee05bedc3e21c100cf26fa2c823461ac90b
Arshia Ray <arshiaray@gmail.com>
2026-08-20 19:55:27 +0100

    built category finder and category meal processor functions

--- ingest.py
+++ ingest.py
@@ -62,3 +62,74 @@ def extract_ingredients(details: dict) -> list[dict]:
             })
 
     return ingredients
+
+
+def fetch_categories() -> list[str]: #free api has a request limit, so category is the best way to get bulk meal recipes
+    """Fetches a list of all the categories in the MealDB API"""
+    url = "https://www.themealdb.com/api/json/v1/1/list.php?c=list"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        print(f"Failed to get categories list from API. Status: {response.status_code}")
+        return []
+
+    data = response.json()
+    categories_dict = data.get("meals", []) #fetches dict of a dict of categories
+
+    # Extract category names into a list
+    categories = [cat["strCategory"] for cat in categories_dict if cat.get("strCategory")]
+    print (f" Discovered {len(categories)} categories from API: {', '.join(categories)}")
+
+    return categories
+
+
+def fetch_store_meals(category: str, cursor, upsert_query): 
+    """Fetches and upserts meals for one category"""
+    url = f"https://www.themealdb.com/api/json/v1/1/filter.php?c={category}"
+    print(f"Processing category: {category}...")
+
+    response = requests.get(url)
+    if response.status_code != 200:
+        print(f"Skipping category {category} due to API error. Status: {response.status_code}")
+        return 0
+
+    meals_summary = response.json().get("meals', [])
+    if not meals_summary:
+        print(f"No meals found for category {category}.")
+        return 0
+
+    meals_processed = 0
+    for meal in meals_summary:
+        meal_id = meal_item["idMeal']
+        detail_url = f"https://www.themealdb.com/api/json/v1/1/lookup.php?i={meal_id}"
+        detail_response = requests.get(detail_url)
+
+        if detail_response.status_code != 200:
+            print(f"Skipping meal {meal_id} due to API error. Status: {detail_response.status_code}")
+            continue
+        else:
+            details = detail_response.json().get("meals", [{}])[0] #API returns a list of one dict, so we take the first element
+            ingredients_list = extract_ingredients(details)
+
+            cursor.execute(upsert_query, (
+                details.get("idMeal"),
+                details.get("strMeal"),
+                details.get("strCategory"),
+                details.get("strArea"),
+                details.get("strInstructions"),
+                json.dumps(ingredients_list),  # Convert list of dicts to JSON string
+                details.get("strMealThumb"),
+                ),
+            )
+            meals_processed += 1
+
+            #small delay to avoid spamming the API with requests
+            time.sleep(0.1)
+
+    print(f"Finished processing category: {category}. Processed {meals_processed} recipes.")
+    return meals_processed
+
+    
+
+
+    
